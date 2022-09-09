cf9dd9bb04a55b2aed158318afd19fada1b4176b
Oleksandr <alex505osip@gmail.com>
2022-09-09 11:25:00 +0300

    unit_test_passed

--- app/src/main/java/com/example/lunchtray/MainActivity.kt
+++ app/src/main/java/com/example/lunchtray/MainActivity.kt
@@ -19,8 +19,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.lunchtray.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
@@ -38,9 +36,5 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
         // TODO: Retrieve NavController from the NavHostFragment
 
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
